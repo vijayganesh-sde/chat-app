77e81609d6a6c8f02648414312d198938df35971
vijayganesh-sde <93527877+vijayganesh-sde@users.noreply.github.com>
2021-12-21 00:06:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/', function(req, res){
+    res.sendFile(__dirname+'/templates/index.html');
+});
+users=[];
+io.on('connection', (socket) => {
+    console.log("connected..");
+    socket.on('setUsername', (data) =>{
+        console.log(data);
+        users.push(data);
+        socket.emit('userSet', {username:data});    
+    });
+    socket.on('msg', (data) =>{
+        io.sockets.emit('newmsg', data);
+    })
+});
+
+http.listen(3000, () => {
+    console.log("listening on 3000");
+});
