dcffac2cb9e6b57b5beb1b24cbcf34f41feb5dc3
vijayganesh-sde <93527877+vijayganesh-sde@users.noreply.github.com>
2022-05-15 22:24:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.get('/', function(req, res){
-    res.sendFile(__dirname+'/templates/index.html');
+    res.sendFile(__dirname+'./index.html');
 });
 users=[];
 io.on('connection', (socket) => {
